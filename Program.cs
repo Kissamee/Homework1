3029c67db8593bfd8a868626d70fcbf8397b24c7
bek <faust808-1990@mail.ru>
2022-12-19 07:55:38 +0300

    [exercise1]

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int a = 100;
+int b = 183;
+
+int max = a;
+int min = b;
+
+if(a > max) max = a;
+if(b > max) max = b;
+
+if(a < min) min = a;
+if(b < min) min = b;
+
+Console.Write ("Минимум, ");
+Console.WriteLine (min);
+
+Console.Write ("Максимум, ");
+Console.WriteLine (max);
