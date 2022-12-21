531569d825232ebf99cc06a40d6dad50760723f6
bek <faust808-1990@mail.ru>
2022-12-21 21:59:09 +0300

    [exercise4]

--- Program.cs
+++ Program.cs
@@ -47,18 +47,31 @@ Console.WriteLine("Максимальное из этих чисел " + max);
 Console.WriteLine("Введите число:");
  int num = Convert.ToInt32(Console.ReadLine());
 
-if (num % 2 == 1)
+if (num % 2 == 0)
 {
-    Console.WriteLine(num +   " нечетное");
+    Console.WriteLine(num +   " четное");
 }
 else
 {
-    Console.WriteLine( num + " четное" );
+    Console.WriteLine( num + " нечетное" );
 }
-
 */
+
 
 
 // Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
+/*
+Console.WriteLine("Введите число:");
+ int n = Convert.ToInt32(Console.ReadLine());
+
+int current = 0;
+
+while (current <= n)
+{
+    Console.Write(current + "  ");
+    current = current +2;
+}
+
+*/
 
