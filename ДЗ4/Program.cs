89a398d0b1811e48165822e487aa582a1f8eb5d9
bek <faust808-1990@mail.ru>
2023-01-17 08:15:21 +0300

    [exercise42]

--- ДЗ4/Program.cs
+++ ДЗ4/Program.cs
@@ -3,12 +3,11 @@
 
 // Задача № 1 Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
+/*
 int numberA = ReadInt("Введите число A: ");
 int numberB = ReadInt("Введите число B: ");
 ToDegree(numberA, numberB);
 
-
-// Функция возведения в степень
 void ToDegree(int a, int b)
 {
     int result = 1;
@@ -19,9 +18,47 @@ void ToDegree(int a, int b)
     Console.WriteLine(result);
 }
 
-// Функция ввода
 int ReadInt(string message)
 {
     Console.WriteLine(message);
     return Convert.ToInt32(Console.ReadLine());
+}
+*/
+
+// Задача № 2 Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int number = ReadInt("Введите число: ");
+int len = NumberLen(number);
+SumNumbers(number, len);
+
+
+// Функция ввода
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Функция подсчета цифр в числе
+int NumberLen(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+// Функция вывода суммы цифр в числе
+void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
 }
